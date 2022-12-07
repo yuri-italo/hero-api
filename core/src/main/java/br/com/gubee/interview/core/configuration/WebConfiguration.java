f0efa752321fdd7aef1a1670f7d5099284aba04b
yuri-italo <yuri.italo94@gmail.com>
2022-12-07 09:57:07 -0300

    CORS disable

--- core/src/main/java/br/com/gubee/interview/core/configuration/WebConfiguration.java
+++ core/src/main/java/br/com/gubee/interview/core/configuration/WebConfiguration.java
@@ -1,6 +1,7 @@
 package br.com.gubee.interview.core.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.PostConstruct;
@@ -9,6 +10,12 @@ import java.util.TimeZone;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*");
+    }
 
     @PostConstruct
     void init() {
