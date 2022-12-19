9b37ba7eeba164442be8d26b5b7568e7ca462280
yuri-italo <yuri.italo94@gmail.com>
2022-12-19 13:49:15 -0300

    HeroService Tests using InMemory implementation.

--- core/src/test/java/br/com/gubee/interview/core/features/hero/HeroServiceTest.java
+++ core/src/test/java/br/com/gubee/interview/core/features/hero/HeroServiceTest.java
@@ -7,8 +7,11 @@ import br.com.gubee.interview.model.Hero;
 import br.com.gubee.interview.model.PowerStats;
 import br.com.gubee.interview.model.enums.Race;
 import br.com.gubee.interview.model.request.CreateHeroRequest;
+import br.com.gubee.interview.model.request.UpdateHeroRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -17,13 +20,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HeroServiceTest {
 
+    private HeroRepositoryInMemoryImpl heroRepositoryInMemory;
+    private PowerStatsService powerStatsService;
+    private HeroService heroService;
+
+    @BeforeEach
+    void init() {
+        heroRepositoryInMemory = new HeroRepositoryInMemoryImpl();
+        PowerStatsRepositoryInMemoryImpl powerStatsRepositoryInMemory = new PowerStatsRepositoryInMemoryImpl();
+        powerStatsService = new PowerStatsService(powerStatsRepositoryInMemory);
+        heroService= new HeroService(heroRepositoryInMemory,powerStatsService);
+
+        storageCleaner();
+    }
+
     @Test
     void createShouldCreateANewHeroContainingTheRequestProperties() {
         // given
-        HeroRepositoryInMemoryImpl heroRepositoryInMemory = new HeroRepositoryInMemoryImpl();
-        PowerStatsRepositoryInMemoryImpl powerStatsRepositoryInMemory = new PowerStatsRepositoryInMemoryImpl();
-        PowerStatsService powerStatsService = new PowerStatsService(powerStatsRepositoryInMemory);
-        HeroService heroService = new HeroService(heroRepositoryInMemory,powerStatsService);
         CreateHeroRequest request = createHeroRequest();
 
         // when
@@ -56,12 +69,32 @@ class HeroServiceTest {
     }
 
     @Test
+    void createShouldThrowIllegalArgumentExceptionWhenTryingToCreateTwoHeroesWithSameName() {
+        // given
+        heroService.create(createHeroRequest());
+
+        // when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> heroService.create(createHeroRequest()));
+
+        // then
+        assertNotNull(e);
+    }
+
+    @Test
+    void createShouldThrowIllegalArgumentExceptionWhenRequestNameNotExists() {
+        // given
+        CreateHeroRequest request = createWithoutNameHeroRequest();
+
+        // when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> heroService.create(request));
+
+        // then
+        assertNotNull(e);
+    }
+
+    @Test
     void findByIdShouldReturnAnOptionalWithTheCorrectHeroWhenIdExists() {
         // given
-        HeroRepositoryInMemoryImpl heroRepositoryInMemory = new HeroRepositoryInMemoryImpl();
-        PowerStatsRepositoryInMemoryImpl powerStatsRepositoryInMemory = new PowerStatsRepositoryInMemoryImpl();
-        PowerStatsService powerStatsService = new PowerStatsService(powerStatsRepositoryInMemory);
-        HeroService heroService = new HeroService(heroRepositoryInMemory,powerStatsService);
         CreateHeroRequest request = createHeroRequest();
 
         UUID uuid = heroService.create(request);
@@ -93,17 +126,22 @@ class HeroServiceTest {
     }
 
     @Test
+    void findByIdShouldReturnAnEmptyOptionalWhenIdNotExists() {
+        // given
+        UUID uuid = UUID.randomUUID();
+
+        // when
+        Optional<Hero> optionalHero = heroService.findById(uuid);
+
+        // then
+        assertTrue(optionalHero.isEmpty());
+    }
+
+    @Test
     void findManyByNameShouldReturnAListWithTwoHeroesContainingTheSearchInTheName() {
         // given
-        HeroRepositoryInMemoryImpl heroRepositoryInMemory = new HeroRepositoryInMemoryImpl();
-        PowerStatsRepositoryInMemoryImpl powerStatsRepositoryInMemory = new PowerStatsRepositoryInMemoryImpl();
-        PowerStatsService powerStatsService = new PowerStatsService(powerStatsRepositoryInMemory);
-        HeroService heroService = new HeroService(heroRepositoryInMemory,powerStatsService);
-        CreateHeroRequest request = createHeroRequest();
-        CreateHeroRequest request2 = createAnotherHeroRequest();
-
-        UUID uuid = heroService.create(request);
-        UUID uuid2 = heroService.create(request2);
+        UUID uuid = heroService.create(createHeroRequest());
+        UUID uuid2 = heroService.create(createAnotherHeroRequest());
 
         String search = "man";
 
@@ -112,23 +150,161 @@ class HeroServiceTest {
 
         // then
         assertNotNull(heroes);
+        assertEquals(2,heroes.size());
 
+        Optional<Hero> optionalHero = heroService.findById(uuid);
+        Hero hero = optionalHero.get();
+        assertTrue(hero.getName().contains(search));
+        assertTrue(heroes.contains(hero));
+
+        Optional<Hero> optionalHero2 = heroService.findById(uuid2);
+        Hero hero2 = optionalHero2.get();
+        assertTrue(hero2.getName().contains(search));
+        assertTrue(heroes.contains(hero2));
     }
 
     @Test
-    void findByName() {
+    void findManyByNameShouldReturnAnEmptyListWhenThereAreNoHeroesContainingTheSearchInTheName() {
+        // given
+        UUID uuid = heroService.create(createHeroRequest());
+        UUID uuid2 = heroService.create(createAnotherHeroRequest());
+
+        String search = "INVALID_SEARCH";
+
+        // when
+        List<Hero> heroes = heroService.findManyByName(search);
+
+        // then
+        assertNotNull(heroes);
+        assertEquals(0,heroes.size());
     }
 
     @Test
-    void findAll() {
+    void findByNameShouldReturnAnOptionalWithHeroWhenNameEqualsSearch() {
+        // given
+        heroService.create(createHeroRequest());
+
+        String search = "Batman";
+
+        // when
+        Optional<Hero> optionalHero = heroService.findByName(search);
+
+        // then
+        assertTrue(optionalHero.isPresent());
+
+        Hero hero = optionalHero.get();
+        assertTrue(hero.getName().equalsIgnoreCase(search));
     }
 
     @Test
-    void update() {
+    void findByNameShouldReturnAnEmptyOptionalWhenSearchDoesNotEqualAnyHeroName() {
+        // given
+        heroService.create(createHeroRequest());
+
+        String search = "Spider";
+
+        // when
+        Optional<Hero> optionalHero = heroService.findByName(search);
+
+        // then
+        assertTrue(optionalHero.isEmpty());
     }
 
     @Test
-    void delete() {
+    void findAllShouldReturnAListWithTwoCreatedHeroes() {
+        // given
+        UUID uuid = heroService.create(createHeroRequest());
+        UUID uuid2 = heroService.create(createAnotherHeroRequest());
+
+        // when
+        List<Hero> heroes = heroService.findAll();
+
+        // then
+        assertNotNull(heroes);
+        assertEquals(2,heroes.size());
+
+        Hero hero = heroService.findById(uuid).get();
+        Hero hero2 = heroService.findById(uuid2).get();
+        assertTrue(heroes.contains(hero));
+        assertTrue(heroes.contains(hero2));
+    }
+
+    @Test
+    void findAllShouldReturnAnEmptyListWhenDoesNotExistsCreatedHeroes() {
+        // when
+        List<Hero> heroes = heroService.findAll();
+
+        // then
+        assertNotNull(heroes);
+        assertEquals(0,heroes.size());
+    }
+
+    @Test
+    void updateShouldUpdateHeroFieldsAccordingToRequest() {
+        // given
+        UpdateHeroRequest updateHeroRequest = createUpdateHeroRequest();
+
+        UUID uuid = heroService.create(createHeroRequest());
+
+        Optional<Hero> optionalHero = heroService.findById(uuid);
+        Hero hero = optionalHero.get();
+        Instant heroCreateTimeBeforeUpdate = hero.getCreatedAt();
+        Instant heroUpdateTimeBeforeUpdate = hero.getUpdatedAt();
+
+        PowerStats powerStats = powerStatsService.findById(hero.getPowerStatsId());
+        Instant powerStatsCreateTimeBeforeUpdate = powerStats.getCreatedAt();
+        Instant powerStatsUpdateTimeBeforeUpdate = powerStats.getUpdatedAt();
+
+        // when
+        heroService.update(hero,updateHeroRequest);
+
+        // then
+        assertNotNull(hero.getId());
+        assertEquals(updateHeroRequest.getName(),hero.getName());
+        assertEquals(updateHeroRequest.getRace(),hero.getRace());
+        assertEquals(updateHeroRequest.getEnabled(),hero.isEnabled());
+        assertEquals(heroCreateTimeBeforeUpdate,hero.getCreatedAt());
+        assertNotEquals(heroUpdateTimeBeforeUpdate,hero.getUpdatedAt());
+        assertNotNull(hero.getPowerStatsId());
+
+        assertNotNull(powerStats.getId());
+        assertEquals(updateHeroRequest.getAgility(),powerStats.getAgility());
+        assertEquals(updateHeroRequest.getDexterity(),powerStats.getDexterity());
+        assertEquals(updateHeroRequest.getIntelligence(),powerStats.getIntelligence());
+        assertEquals(updateHeroRequest.getStrength(),powerStats.getStrength());
+        assertEquals(powerStatsCreateTimeBeforeUpdate,powerStats.getCreatedAt());
+        assertNotEquals(powerStatsUpdateTimeBeforeUpdate,powerStats.getUpdatedAt());
+    }
+
+    @Test
+    void deleteShouldDeleteWhenHeroExists() {
+        // given
+        UUID uuid = heroService.create(createHeroRequest());
+        Optional<Hero> optionalHero = heroService.findById(uuid);
+        Hero hero = optionalHero.get();
+
+        // when
+        heroService.delete(hero);
+
+        // then
+        Optional<Hero> deletedHero = heroService.findById(uuid);
+        assertTrue(deletedHero.isEmpty());
+
+        PowerStats deletedPowerStats = powerStatsService.findById(hero.getPowerStatsId());
+        assertNull(deletedPowerStats);
+    }
+
+    @Test
+    void deleteShouldThrowNullPointerExceptionWhenHeroIsNull() {
+        // given
+
+        Hero hero = null;
+
+        // when
+        NullPointerException e = assertThrows(NullPointerException.class, () -> heroService.delete(hero));
+
+        // then
+        assertNotNull(e);
     }
 
     private CreateHeroRequest createHeroRequest() {
@@ -151,5 +327,32 @@ class HeroServiceTest {
                 .intelligence(10)
                 .race(Race.DIVINE)
                 .build();
+    }
+
+    private CreateHeroRequest createWithoutNameHeroRequest() {
+        return CreateHeroRequest.builder()
+                .agility(9)
+                .dexterity(9)
+                .strength(9)
+                .intelligence(9)
+                .race(Race.ALIEN)
+                .build();
+    }
+
+    private UpdateHeroRequest createUpdateHeroRequest() {
+        return UpdateHeroRequest.builder()
+                .name("Thor")
+                .race(Race.DIVINE)
+                .agility(8)
+                .dexterity(8)
+                .intelligence(8)
+                .strength(8)
+                .enabled(false)
+                .build();
+    }
+
+    private void storageCleaner() {
+        heroRepositoryInMemory.findAll()
+                .forEach(hero -> heroRepositoryInMemory.delete(hero));
     }
 }
